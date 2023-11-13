75de2825206c5ed6e24ee87f92bba9f5616305ad
Tomat <xxlennygamerxx@gmail.com>
2023-11-12 21:40:05 -0800

    Clean up OpenWinExt.

--- src/OpenWinExt/dllmain.cpp
+++ src/OpenWinExt/dllmain.cpp
@@ -1,136 +1,136 @@
 #include "dllmain.h"
+
+#include <basetsd.h>
 #include <malloc.h>
-#include <WinUser.h>
 #include <minwindef.h>
 #include <windef.h>
-#include <basetsd.h>
+#include <WinUser.h>
 
+// ReSharper disable once CppInconsistentNaming
 BOOL APIENTRY DllMain(HMODULE, DWORD, LPVOID) { return TRUE; }
 
 EXPORT GM_DOUBLE win_display_count(void)
 {
-	return (GM_DOUBLE)window_display_count;
+    return window_display_count;
 }
 
 EXPORT GM_DOUBLE win_display_get_default_monitor_index(void)
 {
-	return (GM_DOUBLE)default_monitor_index;
+    return default_monitor_index;
 }
 
-EXPORT GM_DOUBLE win_display_get_height(GM_DOUBLE windowIndex)
+EXPORT GM_DOUBLE win_display_get_height(const GM_DOUBLE window_index)
 {
-	if (windowIndex >= 0 && windowIndex < window_display_count)
-		return displays[(int)windowIndex]->rcMonitor.bottom;
+    if (window_index >= 0 && window_index < window_display_count)
+        return displays[static_cast<int>(window_index)]->rcMonitor.bottom;
 
-	return -1.0;
+    return -1.0;
 }
 
-EXPORT GM_DOUBLE win_display_get_width(GM_DOUBLE windowIndex)
+EXPORT GM_DOUBLE win_display_get_width(const GM_DOUBLE window_index)
 {
-	if (windowIndex >= 0 && windowIndex < window_display_count)
-		return displays[(int)windowIndex]->rcMonitor.right;
+    if (window_index >= 0 && window_index < window_display_count)
+        return displays[static_cast<int>(window_index)]->rcMonitor.right;
 
-	return -1.0;
+    return -1.0;
 }
 
-EXPORT GM_DOUBLE win_display_get_x(GM_DOUBLE windowIndex)
+EXPORT GM_DOUBLE win_display_get_x(const GM_DOUBLE window_index)
 {
-	if (windowIndex >= 0 && windowIndex < window_display_count)
-		return displays[(int)windowIndex]->rcMonitor.left;
+    if (window_index >= 0 && window_index < window_display_count)
+        return displays[static_cast<int>(window_index)]->rcMonitor.left;
 
-	return -1.0;
+    return -1.0;
 }
 
-EXPORT GM_DOUBLE win_display_get_y(GM_DOUBLE windowIndex)
+EXPORT GM_DOUBLE win_display_get_y(const GM_DOUBLE window_index)
 {
-	if (windowIndex >= 0 && windowIndex < window_display_count)
-		return displays[(int)windowIndex]->rcMonitor.top;
+    if (window_index >= 0 && window_index < window_display_count)
+        return displays[static_cast<int>(window_index)]->rcMonitor.top;
 
-	return -1.0;
+    return -1.0;
 }
 
 EXPORT GM_DOUBLE win_display_update_cache(void)
 {
-	for (int i = 0; i < window_display_count; i++)
-	{
-		free(displays[i]);
-		displays[i] = {};
-	}
+    for (int i = 0; i < window_display_count; i++)
+    {
+        free(displays[i]);
+        displays[i] = {};
+    }
 
-	// Set cbSize before handle_enum_display_monitors since it uses this as
-	// input and the size controls whether it fills MONITORINFO or
-	// MONITORINFOEX.
-	display.cbSize = sizeof(MONITORINFO); // 40 bytes, 0x28
-	window_display_count = 0;
-	default_monitor_index = 0;
+    // Set cbSize before handle_enum_display_monitors since it uses this as
+    // input and the size controls whether it fills MONITORINFO or
+    // MONITORINFOEX.
+    display.cbSize = sizeof(MONITORINFO); // 40 bytes, 0x28
+    window_display_count = 0;
+    default_monitor_index = 0;
 
-	EnumDisplayMonitors(nullptr, nullptr, handle_enum_display_monitors, 0);
+    EnumDisplayMonitors(nullptr, nullptr, handle_enum_display_monitors, 0);
 
-	return GM_TRUE;
+    return GM_TRUE;
 }
 
 EXPORT GM_STRING win_get_keyboard_layout_name(void)
 {
-	GetKeyboardLayoutNameA(keyboard_layout_name);
-	return keyboard_layout_name;
+    GetKeyboardLayoutNameA(keyboard_layout_name);
+    return keyboard_layout_name;
 }
 
-EXPORT GM_DOUBLE win_vk_to_vsc(GM_DOUBLE uCode)
+EXPORT GM_DOUBLE win_vk_to_vsc(const GM_DOUBLE u_code)
 {
-	HKL hkl = GetKeyboardLayout(0);
-	return (GM_DOUBLE)MapVirtualKeyExA((UINT)uCode, MAPVK_VK_TO_VSC, hkl);
+    const HKL hkl = GetKeyboardLayout(0);
+    return MapVirtualKeyExA(static_cast<UINT>(u_code), MAPVK_VK_TO_VSC, hkl);
 }
 
-EXPORT GM_DOUBLE win_vsc_to_vk(GM_DOUBLE uCode)
+EXPORT GM_DOUBLE win_vsc_to_vk(const GM_DOUBLE u_code)
 {
-	HKL hkl = GetKeyboardLayout(0);
-	return (GM_DOUBLE)MapVirtualKeyExA((UINT)uCode, MAPVK_VSC_TO_VK, hkl);
+    const HKL hkl = GetKeyboardLayout(0);
+    return MapVirtualKeyExA(static_cast<UINT>(u_code), MAPVK_VSC_TO_VK, hkl);
 }
 
-// Parameter is a handle masquerading as a char*.
-EXPORT GM_DOUBLE win_window_set_popup(GM_STRING windowHandle, GM_DOUBLE togglePopup)
+EXPORT GM_DOUBLE win_window_set_popup(void* window_handle, const GM_DOUBLE toggle_popup)
 {
-	GM_DOUBLE success = GM_FALSE;
-	LONG_PTR styles;
-	
-	if (windowHandle)
-	{
-		styles = GetWindowLongPtrW((HWND)windowHandle, GWL_STYLE);
+    GM_DOUBLE success = GM_FALSE;
 
-		if (!togglePopup)
-		{
-			styles &= ~WS_POPUP;
-		}
-		else
-		{
-			styles |= WS_POPUP;
-		}
+    if (window_handle)
+    {
+        LONG_PTR styles = GetWindowLongPtrW(static_cast<HWND>(window_handle), GWL_STYLE);
 
-		SetWindowLongPtrW((HWND)windowHandle, GWL_STYLE, styles);
-		success = GM_TRUE;
-	}
+        if (toggle_popup) // NOLINT(bugprone-narrowing-conversions, clang-diagnostic-float-conversion, cppcoreguidelines-narrowing-conversions)
+        {
+            styles |= WS_POPUP;
+        }
+        else
+        {
+            styles &= ~WS_POPUP;
+        }
 
-	return success;
+        SetWindowLongPtrW(static_cast<HWND>(window_handle), GWL_STYLE, styles);
+        success = GM_TRUE;
+    }
+
+    return success;
 }
 
-BOOL handle_enum_display_monitors(HMONITOR hMonitor, HDC, LPRECT, LPARAM)
+BOOL handle_enum_display_monitors(const HMONITOR monitor, HDC, LPRECT, LPARAM)
 {
-	if (window_display_count < 32 && GetMonitorInfoA(hMonitor, &display))
-	{
-		LPMONITORINFO info = (LPMONITORINFO)malloc(sizeof(MONITORINFO));
+    if (window_display_count < 32 && GetMonitorInfoA(monitor, &display))
+    {
+        const auto info = static_cast<LPMONITORINFO>(malloc(sizeof(MONITORINFO)));
 
-		if (!info)
-			return FALSE;
+        if (!info)
+            return FALSE;
 
-		memcpy(info, &display, sizeof(MONITORINFO));
+        memcpy(info, &display, sizeof(MONITORINFO));
 
-		displays[window_display_count] = info;
+        displays[window_display_count] = info;
 
-		if (info->rcMonitor.left == 0 && info->rcMonitor.top== 0)
-			default_monitor_index = window_display_count;
+        if (info->rcMonitor.left == 0 && info->rcMonitor.top == 0)
+            default_monitor_index = window_display_count;
 
-		window_display_count++;
-	}
+        window_display_count++;
+    }
 
-	return TRUE;
+    return TRUE;
 }
--- src/OpenWinExt/dllmain.h
+++ src/OpenWinExt/dllmain.h
@@ -2,6 +2,7 @@
 
 #include <Windows.h>
 
+// ReSharper disable once CppInconsistentNaming
 BOOL APIENTRY DllMain(HMODULE, DWORD, LPVOID);
 
 #define EXPORT extern "C" __declspec(dllexport)
@@ -18,14 +19,14 @@ static CHAR keyboard_layout_name[KL_NAMELENGTH];
 
 EXPORT GM_DOUBLE win_display_count(void);
 EXPORT GM_DOUBLE win_display_get_default_monitor_index(void);
-EXPORT GM_DOUBLE win_display_get_height(GM_DOUBLE windowIndex);
-EXPORT GM_DOUBLE win_display_get_width(GM_DOUBLE windowIndex);
-EXPORT GM_DOUBLE win_display_get_x(GM_DOUBLE windowIndex);
-EXPORT GM_DOUBLE win_display_get_y(GM_DOUBLE windowIndex);
+EXPORT GM_DOUBLE win_display_get_height(GM_DOUBLE window_index);
+EXPORT GM_DOUBLE win_display_get_width(GM_DOUBLE window_index);
+EXPORT GM_DOUBLE win_display_get_x(GM_DOUBLE window_index);
+EXPORT GM_DOUBLE win_display_get_y(GM_DOUBLE window_index);
 EXPORT GM_DOUBLE win_display_update_cache(void);
 EXPORT GM_STRING win_get_keyboard_layout_name(void);
-EXPORT GM_DOUBLE win_vk_to_vsc(GM_DOUBLE uCode);
-EXPORT GM_DOUBLE win_vsc_to_vk(GM_DOUBLE uCode);
-EXPORT GM_DOUBLE win_window_set_popup(GM_STRING p1, GM_DOUBLE p2);
+EXPORT GM_DOUBLE win_vk_to_vsc(GM_DOUBLE u_code);
+EXPORT GM_DOUBLE win_vsc_to_vk(GM_DOUBLE u_code);
+EXPORT GM_DOUBLE win_window_set_popup(void* window_handle, GM_DOUBLE toggle_popup);
 
-BOOL handle_enum_display_monitors(HMONITOR hMonitor, HDC, LPRECT, LPARAM);
+BOOL handle_enum_display_monitors(HMONITOR monitor, HDC, LPRECT, LPARAM);
