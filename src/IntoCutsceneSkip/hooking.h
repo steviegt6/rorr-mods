287ddabee931f1ed99e85c0b4b538bfa693d4f42
Tomat <xxlennygamerxx@gmail.com>
2023-11-12 19:35:01 -0800

    Murder existing projects.

--- src/IntoCutsceneSkip/hooking.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-void Hook(void* to, void* from, void** orig, char* name);
--- src/Torrential/main.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define YYSDK_PLUGIN
-#include <SDK.hpp>;
-
-DllExport YYTKStatus PluginEntry(YYTKPlugin* pPlugin);
-DllExport YYTKStatus PluginUnload();
